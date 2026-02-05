4397f99ead6ae01ad9b44cff76c0831059c0b3b2
mhidalgoaraya <40123889+mhidalgoaraya@users.noreply.github.com>
2026-02-05 14:12:35 +0000

    Deploying to gh-pages from @ mhidalgoaraya/mhidalgoaraya.github.io@cfecbaa0975e131c4c5a0d0549401c03ee05102a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -100,13 +100,6 @@ ninja.data = [{
           window.open("mailto:%6D%61%72%68%69%61%72@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
